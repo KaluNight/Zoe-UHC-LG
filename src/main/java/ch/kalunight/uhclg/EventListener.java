eaf4b3748d5da2a34f529d4072c72d615d2efd5d
KaluNight <lucadasilva003@gmail.com>
2020-01-08 10:57:57 +0100

    Test event listener

---
+++ src/main/java/ch/kalunight/uhclg/EventListener.java
@@ -0,0 +1,14 @@
+package ch.kalunight.uhclg;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class EventListener implements Listener {
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event)
+  {
+      event.setJoinMessage("Bienvenue sur le serveur " + event.getPlayer().getName() + " !");
+  }
+}
