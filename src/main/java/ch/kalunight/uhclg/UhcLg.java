42c6ca12e9a337eebc78939d587fa5efcf3dd813
Luca Da Silva <luca.dasilva@geo-satis.com>
2020-01-08 10:41:00 +0100

    init

---
+++ src/main/java/ch/kalunight/uhclg/UhcLg.java
@@ -0,0 +1,17 @@
+package ch.kalunight.uhclg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UhcLg extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    
+  }
+  
+  @Override
+  public void onDisable() {
+
+  }
+
+}
