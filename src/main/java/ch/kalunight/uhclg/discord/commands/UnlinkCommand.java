283316ce8cd13282bbd1a9826edf7255e22fff8c
KaluNight <lucadasilva003@gmail.com>
2020-01-10 16:34:11 +0100

    Add new command

---
+++ src/main/java/ch/kalunight/uhclg/discord/commands/UnlinkCommand.java
@@ -0,0 +1,14 @@
+package ch.kalunight.uhclg.discord.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class UnlinkCommand extends Command {
+
+  @Override
+  protected void execute(CommandEvent arg0) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
